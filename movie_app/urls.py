e62d12cf5f3b292f1cf7a10f36421b79aee703bd
user <umar.azlun@gmail.com>
2024-10-03 22:03:03 +0600

    HW1

---
+++ movie_app/urls.py
@@ -0,0 +1,13 @@
+from . import views
+
+from django.urls import path
+
+
+urlpatterns = [
+    path('directors/', views.director_list_api_view, name='director_list'),
+    path('directors/<int:id>/', views.director_details_api_view, name='director_id'),
+    path('movies/', views.movie_list_api_view, name='movie_list'),
+    path('movies/<int:id>/', views.movie_details_api_view, name='movie_id'),
+    path('reviews/', views.review_list_api_view, name='review_list'),
+    path('reviews/<int:id>/', views.review_details_api_view, name='review_id'),
+]
---
+++ movie_app/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+
+from .models import Director, Movie, Review
+from .serializible import (
+    DirectorSerializer,
+    MovieSerializer,
+    ReviewSerializer,
+)
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+@api_view(['GET'])
+def director_list_api_view(request):
+    directors = Director.objects.all()
+    data = DirectorSerializer(instance=directors, many=True).data
+    return Response(data=data)
+
+
+@api_view(['GET'])
+def director_details_api_view(request, id):
+    try:
+        director = Director.objects.get(id=id)
+        data = DirectorSerializer(instance=director).data
+        return Response(data=data)
+    except Director.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND, data={'detail': 'Director does not exist in database'})
+    
+
+@api_view(['GET'])
+def movie_list_api_view(request):
+    movies = Movie.objects.all()
+    data = MovieSerializer(instance=movies, many=True).data
+    return Response(data=data)
+
+
+
+@api_view(['GET'])
+def movie_details_api_view(request, id):
+    try:
+        movie = Movie.objects.get(id=id)
+        data = MovieSerializer(instance=movie).data
+        return Response(data=data)
+    except Movie.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND, data={'detail': 'Movie does not exist in database'})
+    
+
+
+@api_view(['GET'])
+def review_list_api_view(request):
+    reviews = Review.objects.all()
+    data = ReviewSerializer(instance=reviews, many=True).data
+    return Response(data=data)
+
+
+@api_view(['GET'])
+def review_details_api_view(request, id):
+    try:
+        review = Review.objects.get(id=id)
+        data = ReviewSerializer(instance=review).data
+        return Response(data=data)
+    except Review.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND, data={'detail': 'Review does not exist in database'})
